ef9186f60689a866812956965c46be5220834024
Your Name <your@email.com>
2019-04-18 17:48:51 +0000

    looping through new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,18 +22,81 @@ $(document).ready(function() {
     created_at: 1461116232227
   };
 
-  // function renderTweets(tweets) {
-  //   // loops through tweets
-  //   // calls createTweetElement for each tweet
-  //   // takes return value and appends it to the tweets container
-  // }
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: {
+          small:
+            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          regular:
+            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          large:
+            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        handle: "@SirIsaac"
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: {
+          small:
+            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          regular:
+            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          large:
+            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        handle: "@rd"
+      },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    },
+    {
+      user: {
+        name: "Johann von Goethe",
+        avatars: {
+          small:
+            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          regular:
+            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          large:
+            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        handle: "@johann49"
+      },
+      content: {
+        text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      created_at: 1461113796368
+    }
+  ];
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    //   calls createTweetElement for each tweet
+    //   takes return value and appends it to the tweets container
+    for (let tweet of tweets) {
+      if (tweet.user) {
+        $("#new-tweet-container").append(createTweetElement(tweet)); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+      }
+    }
+  }
 
   function createTweetElement(tweet) {
-    const imgElement = tweetData.user.avatars.small;
-    const nameElement = tweetData.user.name;
-    const handleElement = tweetData.user.handle;
-    const contentElement = tweetData.content.text;
-    const timeStampElement = tweetData.created_at;
+    const imgElement = tweet.user.avatars.small;
+    const nameElement = tweet.user.name;
+    const handleElement = tweet.user.handle;
+    const contentElement = tweet.content.text;
+    const timeStampElement = tweet.created_at;
 
     let $img = $("<img>")
       .addClass("avatar")
@@ -53,7 +116,20 @@ $(document).ready(function() {
       .append($img, $name, $handle, $content, $footer);
     return $tweet;
   }
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $("#new-tweet-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-}); // renderTweets(data);
+  renderTweets(data);
+});
+
+// {/* <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script> */}
+
+//   $(function() {
+//     var $button = $("#load-more-posts");
+//     $button.on("click", function() {
+//       console.log("Button clicked, performing ajax call...");
+//       $.ajax("more-posts.html", { method: "GET" }).then(function(
+//         morePostsHtml
+//       ) {
+//         console.log("Success: ", morePostsHtml);
+//         $button.replaceWith(morePostsHtml);
+//       });
+//     });
+//   });
